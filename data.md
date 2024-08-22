c95f8da6e9782fff474db5e91056fdc6cb304e9c
nicory <pedronicory@gmail.com>
2024-08-21 22:49:41 -0300

    initiating createuser route

---
+++ data.md
@@ -0,0 +1,17 @@
+Nome Completo
+Idade
+E-mail
+CPF
+CEP
+Rua
+Número
+Bairro
+Cidade
+Estado
+Número de Telefone
+Sexo
+Estado Civil
+Sua família possui casa própria?
+Renda familiar
+Qual é sua renda pessoal mensal?
+Você tem acesso à internet?
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import express from "express";
 
 import connectDatabase from "./src/db/db.js";
 
+import userRoute from "./src/routes/user.route.js";
+
 dotenv.config();
 
 connectDatabase();
@@ -10,9 +12,7 @@ connectDatabase();
 const app = express();
 
 app.use(express.json());
-app.use("/", (req, res) => {
-  res.send({ msg: "helloworld" });
-});
+app.use("/user", userRoute);
 
 app.listen(process.env.PORT, () =>
   console.log("Server is runnig on port 3000")
